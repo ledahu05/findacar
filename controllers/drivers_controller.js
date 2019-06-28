8e5f6d9ac1d238d11b1efa05203c5a573bc3dfe6
christophe.seguinot <christophe.seguinot@gmail.com>
2019-06-28 10:34:07 +0200

    add geoNear query and controller

--- controllers/drivers_controller.js
+++ controllers/drivers_controller.js
@@ -4,6 +4,29 @@ module.exports = {
     greeting(req, res) {
         res.send({ hi: 'there'});
     },
+    index(req, res, next) {
+        const { lng, lat } = req.query;
+        const point = {
+            type: 'Point',
+            coordinates: [parseFloat(lng), parseFloat(lat)],
+        };
+
+        //console.log(point, 'got request');
+        Driver.aggregate([
+            {
+                $geoNear: { 
+                    near: point,
+                    spherical: true,
+                    maxDistance: 200000,
+                    distanceField: 'dist.calculated'
+                }
+            }])
+            .then((drivers) => {
+                //console.log(drivers, 'found drivers');
+                res.send(drivers);
+            })
+            .catch(next);
+    },
     create(req, res, next) {
         const driverProps = req.body;
         Driver.create(driverProps)
